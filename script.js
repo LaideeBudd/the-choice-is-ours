9e8782ea5556c6ca3700efd9663a68684c852dcd
LaideeBudd <107522099+LaideeBudd@users.noreply.github.com>
2023-05-18 19:22:53 -0500

    responsive navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuItems = document.getElementById("menu-items");
+const menuToggle = () => {
+  if (menuItems.style.display === "none") {
+    menuItems.style.display = "flex";
+  } else {
+    menuItems.style.display = "none";
+  }
+};
